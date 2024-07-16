3900b6727f007839037be0b6ed33a37fa4d501b5
乐π <73387366+xiaoji235@users.noreply.github.com>
2024-07-16 14:00:23 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -2,7 +2,7 @@ import json
 import os
 
 # 使用curl命令获取远程JSON数据
-json_data = os.popen('curl -s <URL_OF_JSON>').read()
+json_data = os.popen('curl -s 'VP_SESSION_ID=6513ba65219d4ea6a81965af7997c1ae&Hm_lvt_42cc1880011538681c48bfa0eb27dee2=1720968508' -H 'Host: eastroc.lokwork.com' -H 'Accept: */*' -H 'Sec-Fetch-Site: same-origin' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: zh-CN,zh-Hans;q=0.9' -H 'Sec-Fetch-Mode: cors' -H 'Content-Type: application/json;' -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.33(0x18002129) NetType/WIFI Language/zh_CN miniProgram/wxa16478da92a9df9b' -H 'Connection: keep-alive' -H 'Referer: https://eastroc.lokwork.com/leap/index.html?gameId=10005' -H 'Cookie: VP_SESSION_ID=6513ba65219d4ea6a81965af7997c1ae; Hm_lvt_42cc1880011538681c48bfa0eb27dee2=1720968508' -H 'Sec-Fetch-Dest: empty' 'https://eastroc.lokwork.com/eastroc-api/web/charmingLeap/getRankByPage?gameId=10005&pageNum=1&pageSize=10'').read()
 
 # 将JSON数据转换为Python字典
 data_dict = json.loads(json_data)
