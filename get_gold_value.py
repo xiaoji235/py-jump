a0b5b0f9b8890c58d405f3d1725417671699bdb8
乐π <73387366+xiaoji235@users.noreply.github.com>
2024-07-16 18:01:35 +0800

    Update get_gold_value.py

--- get_gold_value.py
+++ get_gold_value.py
@@ -37,6 +37,18 @@ data['gold'] = str(gold_value)
 # 如果需要将更新后的字典转回JSON字符串
 new_json_str = json.dumps(data)
 
-os.system('git add gold.json')
-os.system('git commit -m "Update gold json"')
-os.system('git push')
+# 将新的JSON数据保存到文件
+with open('updated_data.json', 'w') as file:
+    file.write(new_json_str)
+
+# 提交新的JSON文件到GitHub仓库
+url = 'https://api.github.com/repos/xiaoji235/py-jump/contents/updated_data.json'
+token = os.getenv('GITHUB_TOKEN')
+headers = {'Authorization': f'token {token}'}
+payload = {
+    'message': 'Update gold value in JSON',
+    'content': new_json_str
+}
+response = requests.put(url, headers=headers, json=payload)
+
+print(response.text)
